a35969acfe7f1b82d4fd5ca8ebaf0b948c21d973
caroline-e-miller <caroline.miller@ymail.com>
2021-01-20 20:23:39 -0600

    pseudocode

---
+++ script.js
@@ -0,0 +1,4 @@
+// define global variables (querySelector, $ stuff)
+// assign addEventListener to all buttons 
+// functions for addition, subtraction, multiplication, etc. (assign addEventListener to each function button)
+// assign addEventListener for answer button and assign answer to the correct place
